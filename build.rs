d70cedab383f5c268507630714e32b724d157e26
Jason Han <jjhan@alum.vassar.edu>
2024-01-08 06:49:30 -0500

    Interop with Objective-C!

--- build.rs
+++ build.rs
@@ -5,15 +5,26 @@ fn main() {
     let dest = env::var("OUT_DIR").unwrap();
 
     println!("cargo:rerun-if-changed=src/cpp/");
+
     let src = ["src/cpp/renderer.cc"];
-    cc::Build::new().cpp(true).flag("-std=c++17").files(src.iter()).compile("mybar");
+    cc::Build::new().cpp(true).flag("-std=c++17").files(src.iter()).compile("cpp");
     let bindings = bindgen::Builder::default()
         .header("src/cpp/wrapper.h")
         .clang_arg("-xc++")
-        .clang_arg("-libc++")
         .generate()
         .expect("Unable to generate bindings");
     bindings
         .write_to_file(Path::new(&dest).join("cpp_bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    let objc_src = ["src/cpp/hi.m"];
+    cc::Build::new().files(objc_src.iter()).compile("objc");
+    let objc_bindings = bindgen::Builder::default()
+        .header("src/cpp/objc_wrapper.h")
+        .clang_arg("-xobjective-c")
+        .generate()
+        .expect("Unable to generate bindings");
+    objc_bindings
+        .write_to_file(Path::new(&dest).join("objc_bindings.rs"))
         .expect("Couldn't write bindings!");
 }
---
+++ src/cpp/hi.m
@@ -0,0 +1,3 @@
+int zoinks() {
+    return 35;
+}
